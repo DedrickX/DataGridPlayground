9715651aa614f3a2eadb13061224b5f195e9d8f3
Stanley <stanislav.gulis@gmail.com>
2023-09-06 13:42:16 +0200

    Fix for editor value updated async from recalculate.

--- src/app/items/data-grid-items/data-grid-items.component.ts
+++ src/app/items/data-grid-items/data-grid-items.component.ts
@@ -2,9 +2,14 @@ import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } f
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { DxDataGridModule } from 'devextreme-angular';
+import { EditorPreparedEvent } from 'devextreme/ui/data_grid';
 import { FormArrayState, SetValueAction } from 'ngrx-forms';
 import { GridItemModel, ItemModel } from '../../form-state/model';
 import { FORM_ID } from '../../form-state/state';
+
+import TextBox from "devextreme/ui/text_box";
+import NumberBox from "devextreme/ui/number_box";
+
 
 @Component({
   selector: 'app-data-grid-items',
@@ -23,6 +28,10 @@ export class DataGridItemsComponent implements OnChanges {
 
   protected gridItems: GridItemModel[] = [];
 
+  currentColumn: string;
+  currentKey: any;
+  editorValue: any;
+  activeEditor: any;
 
   constructor(
     private store: Store,
@@ -37,6 +46,13 @@ export class DataGridItemsComponent implements OnChanges {
           __storeIndex: index,
           ...item
         }));
+
+      if (this.gridItems.length > 0 && this.editorValue) {
+        const newValue = this.gridItems.find(i => i.__storeIndex === this.currentKey)[this.currentColumn]
+        if (newValue != this.editorValue) {
+          this.activeEditor.option("value", newValue);
+        }
+      }
     }
   }
 
@@ -47,6 +63,15 @@ export class DataGridItemsComponent implements OnChanges {
       const action = new SetValueAction(`${FORM_ID}.items.${e.key}.${changedProperty}`, value);
       this.store.dispatch(action);
     }
+  }
+
+  editorPrepared(e: EditorPreparedEvent) {
+    this.currentKey = e.row.key;
+    this.currentColumn = e.dataField;
+    this.editorValue = e.value;
+    this.activeEditor = TextBox.getInstance(e.editorElement);
+    if (!this.activeEditor)
+      this.activeEditor = NumberBox.getInstance(e.editorElement);
   }
 
 }
